4b0b95fe9f3495dff4c23c1947211a211c7a44a4
Ryan McHugh <ryanmmch@gmail.com>
2020-03-05 01:32:19 -0500

    Static projects

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 function addProjects(){
-  var html = "<h3> Projects Added! </h3>"
+  var carberry =
+    "<div class='proj'> CarberryPI: <a href='https://github.com/brohemz/carberry-pi'>https://github.com/brohemz/carberry-pi</a> </div>"
 
-  document.getElementById("projects").innerHTML = html;
+  var image_search =
+    "<div class='proj'> image_search: <a href='https://github.com/brohemz/image_search'>https://github.com/brohemz/image_search</a> </div>"
+
+  var movies_app =
+    "<div class='proj'> movies_app: <a href='https://github.com/brohemz/movies-app'>https://github.com/brohemz/movies-app</a> </div>"
+
+  var plagiarism_checker =
+    "<div class='proj'> plagiarism_checker: <a href='https://github.com/brohemz/Program_Plagiarism_Checker'>https://github.com/brohemz/Program_Plagiarism_Checker</a> </div>"
+
+  document.getElementById("projects").innerHTML = carberry + image_search + movies_app + plagiarism_checker;
+}
+
+function addHello(id){
+  document.getElementById(id).innerHTML = "HELLO";
+  console.log(id)
+}
+
+window.onload = () => {
+  console.log("wow")
 }
